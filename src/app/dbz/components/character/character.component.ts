fed525a91e03a9adb2f669b555e58a88eee6ac5c
Alexander <alexnadervelarde2511@gmail.com>
2025-01-12 22:09:40 -0400

    se uso nfor y decorador input

---
+++ src/app/dbz/components/character/character.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'dbz-add-character',
+  standalone: false,
+
+  templateUrl: './character.component.html',
+  styleUrl: './character.component.css'
+})
+export class CharacterComponent {
+
+}
