6f7716d75a939f7325900a53dd56c642f0a6a0c1
Alexander <alexnadervelarde2511@gmail.com>
2025-01-13 01:43:03 -0400

    uuid

--- src/app/dbz/services/dbz.service.ts
+++ src/app/dbz/services/dbz.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Character } from '../interfaces/character.interface';
+import { v4 as uuid } from 'uuid';
 
 @Injectable({
   providedIn: 'root'
@@ -8,19 +9,25 @@ export class DbzService {
 
   public characters: Character[] = [
     {
+      id: uuid(),
       name: 'krilin',
       power: 1000,
     },{
+      id: uuid(),
       name: 'Goku',
       power: 9500,
     },{
+      id: uuid(),
       name: 'Vegeta',
       power: 7000,
     }
+
   ];
 
   onNewCharacter( character: Character ):void {
-    this.characters.push(character);
+    const newCharacter: Character = { id: uuid(), ...character }
+    this.characters.push(newCharacter);
+    console.log(this.characters);
   }
 
   onDeleteCharacter( character: number ):void {
